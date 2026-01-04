4866cf0f781b8dd7e1cd310c1d2e69f0a9337ea7
Dum2601 <Doug_b_souza@hotmail.com>
2026-01-04 13:30:26 -0300

    new: stard die logic added

---
+++ src/dice_logic/die.py
@@ -0,0 +1,29 @@
+from random import randint
+
+class Die:
+    def __init__(self, sides: int = 6, quantity: int = 1):
+        self.sides = sides
+        self.quantity = quantity
+
+    def _dice_value(self) -> int:
+        return randint(1, self.sides)
+
+    def _all_dice_values(self) -> list[int]:
+        values: list[int] = []
+        for _ in range(self.quantity):
+            values.append(self._dice_value())
+        return values
+
+    def _rolling_dices(self) -> list[int]:
+        return self._all_dice_values()
+    @property
+    def die_result(self) -> list[int]:
+        return self._rolling_dices()
+
+
+class RpgDices(Die):
+    pass
+
+
+die = Die().die_result
+print(die)
